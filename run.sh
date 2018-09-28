2443e37570660b87dab5e187fda5e11c9b199d31
Mark Pictor <mpictor@gmail.com>
2018-09-28 11:59:21 -0500

    sandbox

--- run.sh
+++ run.sh
@@ -1,1 +1,3 @@
-bazel build --config=default hello
+echo run one of the following
+echo bazel build --config=docker-sandbox --config=docker-sandbox-crosstool --verbose_failures hello
+echo bazel build --config=docker-sandbox --config=cross --verbose_failures hello
